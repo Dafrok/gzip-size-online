86d32c0233529233883df63e4c6cf3517f7b57c2
马金花儿 <o.o@mug.dog>
2018-03-07 11:17:47 +0800

    add featrue of upload files

--- static/main.js
+++ static/main.js
@@ -1,5 +1,34 @@
-var $result = document.getElementById('result')
-var $text = document.getElementById('text')
-$text.addEventListener('input', function (e) {
-    $result.innerHTML = gzip.zip(e.target.value).length
+var $result = $('#result')
+var $text = $('#text')
+var $file = $('#file')
+
+function $(selector) {
+    return document.querySelector(selector)
+}
+
+function on($el, event, callback) {
+    return $el.addEventListener(event, callback)
+}
+
+function showResult(value) {
+    $result.innerHTML = [value.length, gzip.zip(value).length].join(',')
+}
+
+var fileReader = new FileReader()
+
+on(fileReader, 'loadend', function (e) {
+    $text.value = e.target.result
+    showResult(e.target.result)
+})
+
+on($text, 'input', function (e) {
+    showResult(e.target.value)
+})
+// on($text, 'change', function (e) {
+//     showResult(e.target.value)
+// })
+
+on($file, 'change', function (e) {
+    var file = e.target.files[0]
+    fileReader.readAsText(file)
 })
